910ee9ca6ba4b248534dfee02e43655bb4fdf08b
John Bieling <john.bieling@gmx.de>
2016-11-30 23:51:04 +0100

    strip pref
    Remove all password related functions, password will be asked for on
    401, not in prefs.

--- content/pref.js
+++ content/pref.js
@@ -8,88 +8,45 @@ if (typeof tzpush === "undefined") {
 
 var tzpush = {
     prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.tzpush."),
-    hthost: "",
-    PASSWORD: "",
+/*    hthost: "",
+//    PASSWORD: "",
     SERVER: "",
     USER: "",
-    NEWPASSWORD: "",
-    select: "",
+//    NEWPASSWORD: "",
+    select: "",*/
 
     onopen: function() {
-        this.updateprefs();
-        document.getElementById('passbox').value = this.PASSWORD;
+//        this.updateprefs();
+//        document.getElementById('passbox').value = this.PASSWORD;
+        //Check, if a new deviceID needs to be generated
+        if (this.prefs.getCharPref("deviceId") === "") this.prefs.setCharPref("deviceId", Date.now());
         this.localAbs();
     },
 
     onclose: function() {
     },
 
-    getpassword: function() {
-        var SSL = this.prefs.getBoolPref("https");
-        var host = this.prefs.getCharPref("host");
-        var USER = this.prefs.getCharPref("user");
-        var hthost = "http://" + host;
-        var SERVER = "http://" + host + "/Microsoft-Server-ActiveSync";
-        if (SSL === true) {
-            hthost = "https://" + host;
-            SERVER = "https://" + host + "/Microsoft-Server-ActiveSync";
-        }
+/*    getpassword: function() {
+        let USER = this.prefs.getCharPref("user");
+        let HOST = "http://";
+        if (this.prefs.getBoolPref("https") === true) HOST = "https://";
 
-        var myLoginManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager);
-        var logins = myLoginManager.findLogins({}, hthost, SERVER, null);
-        var password = '';
-        for (var i = 0; i < logins.length; i++) {
+        HOST = HOST + this.prefs.getCharPref("host");
+        let SERVER = HOST + "/Microsoft-Server-ActiveSync";
+
+        let myLoginManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager);
+        let logins = myLoginManager.findLogins({}, HOST, SERVER, null);
+        for (let i = 0; i < logins.length; i++) {
             if (logins[i].username === USER) {
-                password = logins[i].password;
-                break;
+                return logins[i].password;
             }
         }
 
-        if (typeof password === 'undefined') {
-            password = "";
-        }
-        return password;
-    },
+        //uups, no password found - we should ask for it - this will be triggered by the 401 AUTH Error
+        return "";
+    }, */
 
-    setpassword: function() {
-        var SSL = this.prefs.getBoolPref("https");
-        var host = this.prefs.getCharPref("host");
-        var USER = this.prefs.getCharPref("user");
-        var hthost = "http://" + host;
-        var SERVER = "http://" + host + "/Microsoft-Server-ActiveSync";
-        if (SSL === true) {
-            hthost = "https://" + host;
-            SERVER = "https://" + host + "/Microsoft-Server-ActiveSync";
-        }
-        this.PASSWORD = this.getpassword();
-        if (this.NEWPASSWORD !== this.PASSWORD) {
-
-            var nsLoginInfo = new Components.Constructor(
-                "@mozilla.org/login-manager/loginInfo;1",
-                Components.interfaces.nsILoginInfo,
-                "init");
-            var loginInfo = new nsLoginInfo(hthost, SERVER, null, USER, this.PASSWORD, "USER", "PASSWORD");
-
-            var updateloginInfo = new nsLoginInfo(hthost, SERVER, null, USER, this.NEWPASSWORD, "USER", "PASSWORD");
-            var myLoginManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager);
-
-            if (this.NEWPASSWORD !== '') {
-                if (this.NEWPASSWORD !== this.PASSWORD) {
-                    if (this.PASSWORD !== '') {
-                        myLoginManager.removeLogin(loginInfo);
-                    }
-                }
-                myLoginManager.addLogin(updateloginInfo);
-                this.updateprefs();
-            } else if (this.PASSWORD === "" || typeof this.PASSWORD === 'undefined') {
-                myLoginManager.addLogin(updateloginInfo);
-            } else {
-                myLoginManager.removeLogin(loginInfo);
-            }
-        }
-    },
-
-    updateprefs: function() {
+/*    updateprefs: function() {
 
         var addressUrl = this.prefs.getCharPref("abname");
         var SSL = this.prefs.getBoolPref("https");
@@ -103,8 +60,8 @@ var tzpush = {
         }
 
         this.USER = this.prefs.getCharPref("user");
-        this.PASSWORD = this.getpassword();
-        this.NEWPASSWORD = '';
+//        this.PASSWORD = this.getpassword();
+//        this.NEWPASSWORD = ''; 
 
         var deviceId = this.prefs.getCharPref("deviceId");
         if (deviceId === "") {
@@ -113,16 +70,19 @@ var tzpush = {
         }
         var polkey = this.prefs.getCharPref("polkey");
         var synckey = this.prefs.getCharPref("synckey");
-    },
+    }, */
 
     localAbs: function() {
-        var count = -1;
+        //clear list of address books
+        let count = -1;
         while (document.getElementById('localContactsFolder').children.length > 0) {
             document.getElementById('localContactsFolder').removeChild(document.getElementById('localContactsFolder').firstChild);
         }
+
+        //fill list of address books - ignore LDAP, Mailinglists and history
+        let selected = -1;
         let abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
         let allAddressBooks = abManager.directories;
-
         while (allAddressBooks.hasMoreElements()) {
             let addressBook = allAddressBooks.getNext();
             if (addressBook instanceof Components.interfaces.nsIAbDirectory && !addressBook.isRemote && !addressBook.isMailList && addressBook.fileName !== 'history.mab') {
@@ -130,43 +90,43 @@ var tzpush = {
                 ab.setAttribute('label', addressBook.dirName);
                 ab.setAttribute('value', addressBook.URI);
                 count = count + 1;
+
+                //is this book the selected one? 
                 if (this.prefs.getCharPref('abname') === addressBook.URI) {
-                    this.select = count;
+                    selected = count;
                 }
                 document.getElementById('localContactsFolder').appendChild(ab);
             }
         }
 
-        if (this.select !== -1) {
-            document.getElementById('localContactsFolder').selectedIndex = this.select;
-        }
+        if (selected !== -1) document.getElementById('localContactsFolder').selectedIndex = selected;
     },
 
     reset: function() {
-        var addressUrl = this.prefs.getCharPref("abname");
         this.prefs.setCharPref("polkey", '0');
         this.prefs.setCharPref("folderID", "");
         this.prefs.setCharPref("synckey", "");
         this.prefs.setCharPref("LastSyncTime", "-1");
-        this.prefs.setCharPref("deviceId", "");
+        this.prefs.setCharPref("deviceId", Date.now());
         this.prefs.setCharPref("autosync", "0");
 
+        /* Clear ServerId and LastModDate of all cards in addressbook selected for sync - WHY ??? */
         var abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
-        var addressBook = abManager.getDirectory(addressUrl);
-        var card;
+        var addressBook = abManager.getDirectory(this.prefs.getCharPref("abname"));
         var cards = addressBook.childCards;
         while (cards.hasMoreElements()) {
-            card = cards.getNext();
+            let card = cards.getNext();
             if (card instanceof Components.interfaces.nsIAbCard) {
                 card.setProperty('ServerId', '');
                 card.setProperty("LastModifiedDate", '');
                 addressBook.modifyCard(card);
             }
         }
+
+        /* Looks like a cleanup of cards marked for cleanup - WHY ??? */
         Components.utils.import("resource://gre/modules/FileUtils.jsm");
         var file = FileUtils.getFile("ProfD", ["DeletedCards"]);
         var entries = file.directoryEntries;
-
         while (entries.hasMoreElements()) {
             var entry = entries.getNext();
             entry.QueryInterface(Components.interfaces.nsIFile);
@@ -184,13 +144,11 @@ var tzpush = {
         } else {
             this.prefs.setCharPref("go", "0");
         }
-    },
+    }, 
 
     cape: function() {
         function openTBtab(tempURL) {
-
             var tabmail = null;
-
             var mail3PaneWindow =
                 Components.classes["@mozilla.org/appshell/window-mediator;1"]
                 .getService(Components.interfaces.nsIWindowMediator)
@@ -210,9 +168,7 @@ var tzpush = {
 
     notes: function() {
         function openTBtab(tempURL) {
-
             var tabmail = null;
-
             var mail3PaneWindow =
                 Components.classes["@mozilla.org/appshell/window-mediator;1"]
                 .getService(Components.interfaces.nsIWindowMediator)
@@ -230,10 +186,10 @@ var tzpush = {
         openTBtab("chrome://tzpush/content/notes.html");
     },
 
-    updatepass: function() {
+/*    updatepass: function() {
         this.NEWPASSWORD = document.getElementById('passbox').value;
         this.setpassword();
-    },
+    }, */
 
     setselect: function(value) {
         this.prefs.setCharPref('abname', value);
