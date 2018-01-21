b408e7382348ce5f50c7e5bc1efd445ca32f8dc2
John Bieling <john.bieling@gmx.de>
2018-01-21 01:24:20 +0100

    cleanup

--- content/provider/eas/tasksync.js
+++ content/provider/eas/tasksync.js
@@ -39,18 +39,11 @@ eas.sync.Tasks = {
 
     /*
 
-    tasks is using extended ISO 8601 (2019-01-18T00:00:00.000Z)  instead of basic (20190118T000000Z)
-
-    item.dueDate is calIDateTime (http://doxygen.db48x.net/mozilla-full/html/d0/d83/interfacecalIDateTime.html)
-    var date = Components.classes["@mozilla.org/calendar/datetime;1"].createInstance(Components.interfaces.calIDateTime);
-    date.icalString = "20080907T120000Z";
-
-    Complete = [0]
     ReminderSet = [0]
-
-    ReminderTime = [2018-01-31T07:00:00.000Z]
     ReminderSet = [1]
+    ReminderTime = [2018-01-31T07:00:00.000Z]
     
+    Complete = [0]
     Complete = [1]
     DateCompleted = [2018-01-31T23:00:00.000Z] //UTC
         
@@ -68,8 +61,12 @@ eas.sync.Tasks = {
         wbxml.atag("Sensitivity", eas.sync.mapThunderbirdPropertyToEas("CLASS", "Sensitivity", item));
         wbxml.atag("Importance", eas.sync.mapThunderbirdPropertyToEas("PRIORITY", "Importance", item));
 
+        //tasks is using extended ISO 8601 (2019-01-18T00:00:00.000Z)  instead of basic (20190118T000000Z), 
+        //getEasTimeUTC returns extended if true as second parameter is present
         wbxml.atag("UtcStartDate", tbSync.eas.getEasTimeUTC(item.entryDate, true));
+        //to fake the local time as UTC, getEasTimeUTC needs the third parameter to be true
         wbxml.atag("StartDate", tbSync.eas.getEasTimeUTC(item.entryDate, true, true));
+
         wbxml.atag("UtcDueDate", tbSync.eas.getEasTimeUTC(item.dueDate, true));
         wbxml.atag("DueDate", tbSync.eas.getEasTimeUTC(item.dueDate, true, true));
         
