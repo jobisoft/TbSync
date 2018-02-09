ec81432917675fd0f7723247a7a619c69447db8c
John Bieling <john.bieling@gmx.de>
2018-02-09 12:49:17 +0100

    fix broken hover (issue #33)

--- content/manager/accountManager.js
+++ content/manager/accountManager.js
@@ -13,13 +13,11 @@ var tbSyncAccountManager = {
         const LOAD_FLAGS_NONE = Components.interfaces.nsIWebNavigation.LOAD_FLAGS_NONE;
 	    let sources = ["accounts.xul", "cape.xul", "catman.xul", "help.xul"];
 
-	    //set background of all taps to white
+	    //set active tab (css selector for background color)
         for (let i=0; i<sources.length; i++) {            
-            document.getElementById("tbSyncAccountManager.t" + i).style.backgroundColor = "#ffffff";
+            if (i==t) document.getElementById("tbSyncAccountManager.t" + i).setAttribute("active","true");
+            else document.getElementById("tbSyncAccountManager.t" + i).setAttribute("active","false");
 	    }
-
-	    //set background of selected tap to highlight
-        document.getElementById("tbSyncAccountManager.t" + t).style.backgroundColor = "#c1d2ee";
 	    
 	    //load XUL
         document.getElementById("tbSyncAccountManager.contentWindow").webNavigation.loadURI("chrome://tbsync/content/manager/"+sources[t], LOAD_FLAGS_NONE, null, null, null);
