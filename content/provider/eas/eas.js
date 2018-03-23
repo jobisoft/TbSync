9a9dba5f08024a95b9464050b87198e25b5b9575
John Bieling <john.bieling@gmx.de>
2018-03-23 21:24:50 +0100

    add description for sync status 5

--- content/provider/eas/eas.js
+++ content/provider/eas/eas.js
@@ -1478,6 +1478,9 @@ var eas = {
             case "Sync:4":
                 throw eas.finishSync("ServerRejectedRequest", eas.flags.abortWithError);                            
             
+            case "Sync:5":
+                throw eas.finishSync("TempServerError", eas.flags.abortWithError);                            
+
             case "Sync:6":
                 //Server does not accept one of our items or the entire request. IF allowSoftFail is set, continue syncing
                 if (allowSoftFail) return false;
