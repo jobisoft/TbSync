eda624043e7d61c9c939637df506d9fef7555557
John Bieling <john.bieling@gmx.de>
2018-03-31 22:52:56 +0200

    fix copy-paste left over

--- content/manager/accountManager.js
+++ content/manager/accountManager.js
@@ -14,15 +14,15 @@ var tbSyncAccountManager = {
 
     selectTab: function (t) {
         const LOAD_FLAGS_NONE = Components.interfaces.nsIWebNavigation.LOAD_FLAGS_NONE;
-	    let sources = ["accounts.xul", "cape.xul", "catman.xul", "supporter.xul", "help.xul"];
+        let sources = ["accounts.xul", "cape.xul", "catman.xul", "supporter.xul", "help.xul"];
 
-	    //set active tab (css selector for background color)
+        //set active tab (css selector for background color)
         for (let i=0; i<sources.length; i++) {            
             if (i==t) document.getElementById("tbSyncAccountManager.t" + i).setAttribute("active","true");
             else document.getElementById("tbSyncAccountManager.t" + i).setAttribute("active","false");
-	    }
-	    
-	    //load XUL
+        }
+        
+        //load XUL
         document.getElementById("tbSyncAccountManager.contentWindow").webNavigation.loadURI("chrome://tbsync/content/manager/"+sources[t], LOAD_FLAGS_NONE, null, null, null);
     },
     
