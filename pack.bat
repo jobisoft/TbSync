f97dcd45d380e74648450f17ba7967d91121eab2
John Bieling <john.bieling@gmx.de>
2018-03-31 02:30:40 +0200

    bootstraping: update pack.bat

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
 del TbSync.xpi
-"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync.xpi components content defaults locale skin chrome.manifest install.rdf LICENSE.txt README.md
+"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync.xpi content locale skin chrome.manifest install.rdf LICENSE.txt README.md bootstrap.js
 
 
