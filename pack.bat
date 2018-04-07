e023b1d4c1fab5e52ed00014ce4105d85749014f
John Bieling <john.bieling@gmx.de>
2018-04-07 22:09:54 +0200

    upd pack.bat

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
-del TbSync.xpi
-"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync.xpi content locale skin chrome.manifest install.rdf LICENSE.txt README.md bootstrap.js
+del TbSync-beta.xpi
+"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync-beta.xpi content locale skin chrome.manifest install.rdf LICENSE.txt README.md bootstrap.js
 
 
