9609b2afc14c823867775a99d215af76af588053
John Bieling <john.bieling@gmx.de>
2018-07-19 09:15:22 +0200

    adding GPL header to all files

--- content/db.js
+++ content/db.js
@@ -1,4 +1,21 @@
-"use strict";
+/*
+ * This file is part of TbSync.
+ *
+ * TbSync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TbSync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TbSync. If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+ "use strict";
 
 var db = {
 
