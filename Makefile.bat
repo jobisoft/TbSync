0368b6f99ec0b0f2c45bf7076aab448a98c5afbc
John Bieling <john.bieling@gmx.de>
2018-11-02 00:23:04 +0100

    Change License headers to MPL2

--- Makefile.bat
+++ Makefile.bat
@@ -1,18 +1,9 @@
 @echo off
 REM  This file is part of TbSync.
 REM 
-REM  TbSync is free software: you can redistribute it and/or modify
-REM  it under the terms of the GNU General Public License as published by
-REM  the Free Software Foundation, either version 3 of the License, or
-REM  (at your option) any later version.
-REM 
-REM  TbSync is distributed in the hope that it will be useful,
-REM  but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-REM  GNU General Public License for more details.
-REM 
-REM  You should have received a copy of the GNU General Public License
-REM  along with TbSync. If not, see <https://www.gnu.org/licenses/>.
+REM This Source Code Form is subject to the terms of the Mozilla Public
+REM License, v. 2.0. If a copy of the MPL was not distributed with this
+REM file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 del TbSync-beta.xpi
 "C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync-beta.xpi content locale skin chrome.manifest install.rdf LICENSE README.md bootstrap.js
