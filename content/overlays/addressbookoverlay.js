d365505a0c5b45e2e6f5c1497ca5a8d9a673e21b
John Bieling <john.bieling@gmx.de>
2019-02-16 23:53:27 +0100

    Revert "cope with strange tbSync.db not defined error"
    This reverts commit f34e09a739525ec02e8bb7f11edc16272e957045.

--- content/overlays/addressbookoverlay.js
+++ content/overlays/addressbookoverlay.js
@@ -68,7 +68,7 @@ var tbSyncAddressBook = {
         }    
         
         let cards = window.GetSelectedAbCards();
-        if (tbSync && tbSync.db && cards.length == 1) { //TODO: tbSync.db seems not to be avail during inject, if open
+        if (cards.length == 1) {
             let aParentDirURI = window.GetSelectedDirectory();
             let abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
             let selectedBook = abManager.getDirectory(aParentDirURI);
