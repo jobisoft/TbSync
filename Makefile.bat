eebb438fb22d8061b71bb218124370817e64bc09
John Bieling <john.bieling@gmx.de>
2019-08-12 18:01:02 +0200

    prepare update path for TB68 - release beta .8

--- Makefile.bat
+++ Makefile.bat
@@ -6,6 +6,6 @@ REM License, v. 2.0. If a copy of the MPL was not distributed with this
 REM file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 del TbSync-beta.xpi
-"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync-beta.xpi content locale skin chrome.manifest install.rdf LICENSE README.md bootstrap.js
+"C:\Program Files\7-Zip\7zG.exe" a -tzip TbSync-beta.xpi content locale skin chrome.manifest install.rdf LICENSE README.md bootstrap.js manifest.json
 
 
