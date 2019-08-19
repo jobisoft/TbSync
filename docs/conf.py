68b2af8ca1366b3b047de63d4b4f01e8009a3f97
John Bieling <john.bieling@gmx.de>
2019-08-20 01:35:12 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ from recommonmark.parser import CommonMarkParser
 
 extensions = ['sphinx_js']
 root_for_relative_js_paths = '../content'
-js_source_path = ['./passwordPrompt','./modules']
+js_source_path = ['../content/passwordPrompt','../content/modules']
 primary_domain = 'js'
 templates_path = ['templates', '_templates', '.templates']
 source_suffix = ['.rst', '.md']
