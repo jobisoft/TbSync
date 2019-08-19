49f957ef768249137730302c163be2d6f4ef0dba
John Bieling <john.bieling@gmx.de>
2019-08-19 18:07:50 +0200

    trying to get generator working

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from recommonmark.parser import CommonMarkParser
 
 extensions = ['sphinx_js']
-js_source_path = '..'
+js_source_path = '../'
 primary_domain = 'js'
 templates_path = ['templates', '_templates', '.templates']
 source_suffix = ['.rst', '.md']
