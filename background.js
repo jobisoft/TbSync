c5fb733b818d7871014e3c0d390e911c6de86201
John Bieling <john.bieling@gmx.de>
2020-07-09 10:31:04 +0200

    prevent auto updates

--- background.js
+++ background.js
@@ -1,4 +1,11 @@
+function handleUpdateAvailable(details) {
+  console.log("Update available for TbSync");
+}
+
 async function main() {
+  // just by registering this listener, updates will not install until next restart
+  messenger.runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
+
   await messenger.LegacyBootstrap.registerChromeUrl([ ["content", "tbsync", "content/"] ]);
   await messenger.LegacyBootstrap.registerBootstrapScript("chrome://tbsync/content/scripts/bootstrap.js");  
 }
